31e0899e6b7584a32cf9122807fbaa0c8587ae89
JASION <5128582+lujiaqaun@user.noreply.gitee.com>
2021-07-30 10:03:42 +0800

    日志添加颜色区分,方便查看

--- log/log.go
+++ log/log.go
@@ -31,30 +31,49 @@ const (
 )
 
 const (
-	printDebugLevel   = "[debug  ] "
+	printDebugLevel   = "[ debug ] "
 	printReleaseLevel = "[release] "
 	printWarningLevel = "[warning] "
-	printErrorLevel   = "[error  ] "
-	printStackLevel   = "[stack  ] "
-	printFatalLevel   = "[fatal  ] "
+	printErrorLevel   = "[ error ] "
+	printStackLevel   = "[ stack ] "
+	printFatalLevel   = "[ fatal ] "
 )
 
+//(修改者:jq)声明颜色
+const (
+	ColorRed     = uint8(iota + 91)
+	ColorGreen   //	绿
+	ColorYellow  //	黄
+	ColorBlue    // 蓝
+	ColorMagenta //	洋红
+)
+
+//(修改者:jq)定义日志级别颜色
+var levelsColor = []uint8{
+	ColorBlue,
+	ColorGreen,
+	ColorYellow,
+	ColorRed,
+	ColorMagenta,
+	ColorMagenta,
+}
+
 type Logger struct {
-	filePath   string
-	filepre 	string
-	
+	filePath string
+	filepre  string
+
 	//logTime    time.Time
-	fileDay        int
-	level      int
-	flag       int
-	buf            []Buffer
+	fileDay int
+	level   int
+	flag    int
+	buf     []Buffer
 
-	outFile    io.Writer  // destination for output
-	outConsole io.Writer  //os.Stdout
+	outFile    io.Writer // destination for output
+	outConsole io.Writer //os.Stdout
 
-	mu     sync.Mutex // ensures atomic writes; protects the following fields
+	mu        sync.Mutex // ensures atomic writes; protects the following fields
 	buffIndex uint32
-	buffNum uint32
+	buffNum   uint32
 }
 
 func (logger *Logger) GenDayFile(now *time.Time) error {
@@ -72,7 +91,7 @@ func (logger *Logger) GenDayFile(now *time.Time) error {
 
 	if logger.filePath != "" {
 		var err error
-		logger.outFile,err = os.Create(path.Join(logger.filePath, logger.filepre+filename))
+		logger.outFile, err = os.Create(path.Join(logger.filePath, logger.filepre+filename))
 		if err != nil {
 			return err
 		}
@@ -80,14 +99,14 @@ func (logger *Logger) GenDayFile(now *time.Time) error {
 		if OpenConsole == true {
 			logger.outConsole = os.Stdout
 		}
-	}else{
+	} else {
 		logger.outConsole = os.Stdout
 	}
 
 	return nil
 }
 
-func New(strLevel string, pathName string, filePre string, flag int,buffNum uint32) (*Logger, error) {
+func New(strLevel string, pathName string, filePre string, flag int, buffNum uint32) (*Logger, error) {
 	// level
 	var level int
 	switch strings.ToLower(strLevel) {
@@ -113,25 +132,24 @@ func New(strLevel string, pathName string, filePre string, flag int,buffNum uint
 	logger.filePath = pathName
 	logger.filepre = filePre
 	logger.flag = flag
-	logger.buf = make([]Buffer,buffNum)
+	logger.buf = make([]Buffer, buffNum)
 	logger.buffNum = buffNum
 
-	for i:=uint32(0);i<buffNum;i++{
+	for i := uint32(0); i < buffNum; i++ {
 		logger.buf[i].Init()
 	}
 
 	now := time.Now()
 	err := logger.GenDayFile(&now)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 
 	return logger, nil
 }
 
-
-func (logger *Logger) nextBuff() *Buffer{
-	return &logger.buf[atomic.AddUint32(&logger.buffIndex,1)%logger.buffNum]
+func (logger *Logger) nextBuff() *Buffer {
+	return &logger.buf[atomic.AddUint32(&logger.buffIndex, 1)%logger.buffNum]
 }
 
 // It's dangerous to call the method on logging
@@ -148,23 +166,26 @@ func (logger *Logger) doPrintf(level int, printLevel string, format string, a ..
 	}
 	now := time.Now()
 
-
 	buf := logger.nextBuff()
 	buf.Locker()
-
+	color := levelsColor[level] //(修改者:jq)
 	buf.Reset()
-	logger.formatHeader(buf,3,&now)
+	buf.AppendString(fmt.Sprintf("\x1b[%dm", color)) //(修改者:jq)
+	buf.AppendString("[")                            //(修改者:jq)
+	logger.formatHeader(buf, 3, &now)
+	buf.AppendString("]") //(修改者:jq)
 	buf.AppendString(printLevel)
+	buf.AppendString("\x1b[0m") //(修改者:jq)
 	buf.AppendString(fmt.Sprintf(format, a...))
 	buf.AppendByte('\n')
 
 	logger.mu.Lock()
 	logger.GenDayFile(&now)
 
-	if logger.outFile!= nil {
+	if logger.outFile != nil {
 		logger.outFile.Write(buf.Bytes())
 	}
-	if logger.outConsole!= nil {
+	if logger.outConsole != nil {
 		logger.outConsole.Write(buf.Bytes())
 	}
 	logger.mu.Unlock()
@@ -174,8 +195,8 @@ func (logger *Logger) doPrintf(level int, printLevel string, format string, a ..
 	}
 }
 
-
 func (logger *Logger) doSPrintf(level int, printLevel string, a []interface{}) {
+	color := levelsColor[level] //(修改者:jq)
 	if level < logger.level {
 		return
 	}
@@ -183,24 +204,28 @@ func (logger *Logger) doSPrintf(level int, printLevel string, a []interface{}) {
 	buf := logger.nextBuff()
 	buf.Locker()
 	buf.Reset()
-	logger.formatHeader(buf,3,&now)
+	buf.AppendString(fmt.Sprintf("\x1b[%dm", color)) //(修改者:jq)
+	buf.AppendString("[")                            //(修改者:jq)
+	logger.formatHeader(buf, 3, &now)
+	buf.AppendString("]") //(修改者:jq)
 	buf.AppendString(printLevel)
-	for _,s := range a {
+	buf.AppendString("\x1b[0m")
+	for _, s := range a {
 		switch s.(type) {
 		//case error:
 		//	logger.buf.AppendString(s.(error).Error())
 		case []string:
 			strSlice := s.([]string)
 			buf.AppendByte('[')
-			for _,str := range strSlice {
+			for _, str := range strSlice {
 				buf.AppendString(str)
 				buf.AppendByte(',')
 			}
 
-			lastIdx := buf.Len()-1
+			lastIdx := buf.Len() - 1
 			if buf.Bytes()[lastIdx] == ',' {
 				buf.Bytes()[lastIdx] = ']'
-			}else{
+			} else {
 				buf.AppendByte(']')
 			}
 
@@ -209,15 +234,15 @@ func (logger *Logger) doSPrintf(level int, printLevel string, a []interface{}) {
 		case []int:
 			intSlice := s.([]int)
 			buf.AppendByte('[')
-			for _,v := range intSlice {
+			for _, v := range intSlice {
 				buf.AppendInt(int64(v))
 				buf.AppendByte(',')
 			}
 
-			lastIdx := buf.Len()-1
+			lastIdx := buf.Len() - 1
 			if buf.Bytes()[lastIdx] == ',' {
 				buf.Bytes()[lastIdx] = ']'
-			}else{
+			} else {
 				buf.AppendByte(']')
 			}
 		case int8:
@@ -225,15 +250,15 @@ func (logger *Logger) doSPrintf(level int, printLevel string, a []interface{}) {
 		case []int8:
 			intSlice := s.([]int8)
 			buf.AppendByte('[')
-			for _,v := range intSlice {
+			for _, v := range intSlice {
 				buf.AppendInt(int64(v))
 				buf.AppendByte(',')
 			}
 
-			lastIdx := buf.Len()-1
+			lastIdx := buf.Len() - 1
 			if buf.Bytes()[lastIdx] == ',' {
 				buf.Bytes()[lastIdx] = ']'
-			}else{
+			} else {
 				buf.AppendByte(']')
 			}
 		case int16:
@@ -241,15 +266,15 @@ func (logger *Logger) doSPrintf(level int, printLevel string, a []interface{}) {
 		case []int16:
 			intSlice := s.([]int16)
 			buf.AppendByte('[')
-			for _,v := range intSlice {
+			for _, v := range intSlice {
 				buf.AppendInt(int64(v))
 				buf.AppendByte(',')
 			}
 
-			lastIdx := buf.Len()-1
+			lastIdx := buf.Len() - 1
 			if buf.Bytes()[lastIdx] == ',' {
 				buf.Bytes()[lastIdx] = ']'
-			}else{
+			} else {
 				buf.AppendByte(']')
 			}
 		case int32:
@@ -257,15 +282,15 @@ func (logger *Logger) doSPrintf(level int, printLevel string, a []interface{}) {
 		case []int32:
 			intSlice := s.([]int32)
 			buf.AppendByte('[')
-			for _,v := range intSlice {
+			for _, v := range intSlice {
 				buf.AppendInt(int64(v))
 				buf.AppendByte(',')
 			}
 
-			lastIdx := buf.Len()-1
+			lastIdx := buf.Len() - 1
 			if buf.Bytes()[lastIdx] == ',' {
 				buf.Bytes()[lastIdx] = ']'
-			}else{
+			} else {
 				buf.AppendByte(']')
 			}
 		case int64:
@@ -273,15 +298,15 @@ func (logger *Logger) doSPrintf(level int, printLevel string, a []interface{}) {
 		case []int64:
 			intSlice := s.([]int64)
 			buf.AppendByte('[')
-			for _,v := range intSlice {
+			for _, v := range intSlice {
 				buf.AppendInt(v)
 				buf.AppendByte(',')
 			}
 
-			lastIdx := buf.Len()-1
+			lastIdx := buf.Len() - 1
 			if buf.Bytes()[lastIdx] == ',' {
 				buf.Bytes()[lastIdx] = ']'
-			}else{
+			} else {
 				buf.AppendByte(']')
 			}
 		case uint:
@@ -290,15 +315,15 @@ func (logger *Logger) doSPrintf(level int, printLevel string, a []interface{}) {
 		case []uint:
 			intSlice := s.([]uint)
 			buf.AppendByte('[')
-			for _,v := range intSlice {
+			for _, v := range intSlice {
 				buf.AppendUint(uint64(v))
 				buf.AppendByte(',')
 			}
 
-			lastIdx := buf.Len()-1
+			lastIdx := buf.Len() - 1
 			if buf.Bytes()[lastIdx] == ',' {
 				buf.Bytes()[lastIdx] = ']'
-			}else{
+			} else {
 				buf.AppendByte(']')
 			}
 
@@ -307,15 +332,15 @@ func (logger *Logger) doSPrintf(level int, printLevel string, a []interface{}) {
 		case []uint8:
 			intSlice := s.([]uint8)
 			buf.AppendByte('[')
-			for _,v := range intSlice {
+			for _, v := range intSlice {
 				buf.AppendUint(uint64(v))
 				buf.AppendByte(',')
 			}
 
-			lastIdx := buf.Len()-1
+			lastIdx := buf.Len() - 1
 			if buf.Bytes()[lastIdx] == ',' {
 				buf.Bytes()[lastIdx] = ']'
-			}else{
+			} else {
 				buf.AppendByte(']')
 			}
 
@@ -324,15 +349,15 @@ func (logger *Logger) doSPrintf(level int, printLevel string, a []interface{}) {
 		case []uint16:
 			intSlice := s.([]uint16)
 			buf.AppendByte('[')
-			for _,v := range intSlice {
+			for _, v := range intSlice {
 				buf.AppendUint(uint64(v))
 				buf.AppendByte(',')
 			}
 
-			lastIdx := buf.Len()-1
+			lastIdx := buf.Len() - 1
 			if buf.Bytes()[lastIdx] == ',' {
 				buf.Bytes()[lastIdx] = ']'
-			}else{
+			} else {
 				buf.AppendByte(']')
 			}
 		case uint32:
@@ -340,15 +365,15 @@ func (logger *Logger) doSPrintf(level int, printLevel string, a []interface{}) {
 		case []uint32:
 			intSlice := s.([]uint32)
 			buf.AppendByte('[')
-			for _,v := range intSlice {
+			for _, v := range intSlice {
 				buf.AppendUint(uint64(v))
 				buf.AppendByte(',')
 			}
 
-			lastIdx := buf.Len()-1
+			lastIdx := buf.Len() - 1
 			if buf.Bytes()[lastIdx] == ',' {
 				buf.Bytes()[lastIdx] = ']'
-			}else{
+			} else {
 				buf.AppendByte(']')
 			}
 		case uint64:
@@ -356,44 +381,44 @@ func (logger *Logger) doSPrintf(level int, printLevel string, a []interface{}) {
 		case []uint64:
 			intSlice := s.([]uint64)
 			buf.AppendByte('[')
-			for _,v := range intSlice {
+			for _, v := range intSlice {
 				buf.AppendUint(v)
 				buf.AppendByte(',')
 			}
-			lastIdx := buf.Len()-1
+			lastIdx := buf.Len() - 1
 			if buf.Bytes()[lastIdx] == ',' {
 				buf.Bytes()[lastIdx] = ']'
-			}else{
+			} else {
 				buf.AppendByte(']')
 			}
 		case float32:
-			buf.AppendFloat(float64(s.(float32)),32)
+			buf.AppendFloat(float64(s.(float32)), 32)
 		case []float32:
 			intSlice := s.([]float32)
 			buf.AppendByte('[')
-			for _,v := range intSlice {
-				buf.AppendFloat(float64(v),32)
+			for _, v := range intSlice {
+				buf.AppendFloat(float64(v), 32)
 				buf.AppendByte(',')
 			}
-			lastIdx := buf.Len()-1
+			lastIdx := buf.Len() - 1
 			if buf.Bytes()[lastIdx] == ',' {
 				buf.Bytes()[lastIdx] = ']'
-			}else{
+			} else {
 				buf.AppendByte(']')
 			}
 		case float64:
-			buf.AppendFloat(s.(float64),64)
+			buf.AppendFloat(s.(float64), 64)
 		case []float64:
 			intSlice := s.([]float64)
 			buf.AppendByte('[')
-			for _,v := range intSlice {
-				buf.AppendFloat(v,64)
+			for _, v := range intSlice {
+				buf.AppendFloat(v, 64)
 				buf.AppendByte(',')
 			}
-			lastIdx := buf.Len()-1
+			lastIdx := buf.Len() - 1
 			if buf.Bytes()[lastIdx] == ',' {
 				buf.Bytes()[lastIdx] = ']'
-			}else{
+			} else {
 				buf.AppendByte(']')
 			}
 		case bool:
@@ -401,14 +426,14 @@ func (logger *Logger) doSPrintf(level int, printLevel string, a []interface{}) {
 		case []bool:
 			intSlice := s.([]bool)
 			buf.AppendByte('[')
-			for _,v := range intSlice {
+			for _, v := range intSlice {
 				buf.AppendBool(v)
 				buf.AppendByte(',')
 			}
-			lastIdx := buf.Len()-1
+			lastIdx := buf.Len() - 1
 			if buf.Bytes()[lastIdx] == ',' {
 				buf.Bytes()[lastIdx] = ']'
-			}else{
+			} else {
 				buf.AppendByte(']')
 			}
 		case string:
@@ -417,98 +442,98 @@ func (logger *Logger) doSPrintf(level int, printLevel string, a []interface{}) {
 			val := s.(*int)
 			if val != nil {
 				buf.AppendInt(int64(*val))
-			}else{
+			} else {
 				buf.AppendString("nil<*int>")
 			}
 		case *int8:
 			val := s.(*int8)
 			if val != nil {
 				buf.AppendInt(int64(*val))
-			}else{
+			} else {
 				buf.AppendString("nil<*int8>")
 			}
 		case *int16:
 			val := s.(*int16)
 			if val != nil {
 				buf.AppendInt(int64(*val))
-			}else{
+			} else {
 				buf.AppendString("nil<*int16>")
 			}
 		case *int32:
 			val := s.(*int32)
 			if val != nil {
 				buf.AppendInt(int64(*val))
-			}else{
+			} else {
 				buf.AppendString("nil<*int32>")
 			}
 		case *int64:
 			val := s.(*int64)
 			if val != nil {
 				buf.AppendInt(int64(*val))
-			}else{
+			} else {
 				buf.AppendString("nil<*int64>")
 			}
 		case *uint:
 			val := s.(*uint)
 			if val != nil {
 				buf.AppendUint(uint64(*val))
-			}else{
+			} else {
 				buf.AppendString("nil<*uint>")
 			}
 		case *uint8:
 			val := s.(*uint8)
 			if val != nil {
 				buf.AppendUint(uint64(*val))
-			}else{
+			} else {
 				buf.AppendString("nil<*uint8>")
 			}
 		case *uint16:
 			val := s.(*uint16)
 			if val != nil {
 				buf.AppendUint(uint64(*val))
-			}else{
+			} else {
 				buf.AppendString("nil<*uint16>")
 			}
 		case *uint32:
 			val := s.(*uint32)
 			if val != nil {
 				buf.AppendUint(uint64(*val))
-			}else{
+			} else {
 				buf.AppendString("nil<*uint32>")
 			}
 		case *uint64:
 			val := s.(*uint64)
 			if val != nil {
 				buf.AppendUint(uint64(*val))
-			}else{
+			} else {
 				buf.AppendString("nil<*uint64>")
 			}
 		case *float32:
 			val := s.(*float32)
 			if val != nil {
-				buf.AppendFloat(float64(*val),32)
-			}else{
+				buf.AppendFloat(float64(*val), 32)
+			} else {
 				buf.AppendString("nil<*float32>")
 			}
 		case *float64:
 			val := s.(*float32)
 			if val != nil {
-				buf.AppendFloat(float64(*val),64)
-			}else{
+				buf.AppendFloat(float64(*val), 64)
+			} else {
 				buf.AppendString("nil<*float64>")
 			}
 		case *bool:
 			val := s.(*bool)
 			if val != nil {
 				buf.AppendBool(*val)
-			}else{
+			} else {
 				buf.AppendString("nil<*bool>")
 			}
 		case *string:
 			val := s.(*string)
 			if val != nil {
 				buf.AppendString(*val)
-			}else{
+			} else {
 				buf.AppendString("nil<*string>")
 			}
 		//case []byte:
@@ -518,7 +543,7 @@ func (logger *Logger) doSPrintf(level int, printLevel string, a []interface{}) {
 			//if err != nil {
 			buf.AppendString("<unknown type>")
 			//}else{
-				//logger.buf.AppendBytes(b)
+			//logger.buf.AppendBytes(b)
 			//}
 		}
 	}
@@ -526,10 +551,10 @@ func (logger *Logger) doSPrintf(level int, printLevel string, a []interface{}) {
 
 	logger.mu.Lock()
 	logger.GenDayFile(&now)
-	if logger.outFile!= nil {
+	if logger.outFile != nil {
 		logger.outFile.Write(buf.Bytes())
 	}
-	if logger.outConsole!= nil {
+	if logger.outConsole != nil {
 		logger.outConsole.Write(buf.Bytes())
 	}
 	logger.mu.Unlock()
@@ -564,7 +589,7 @@ func (logger *Logger) Fatal(format string, a ...interface{}) {
 	logger.doPrintf(fatalLevel, printFatalLevel, format, a...)
 }
 
-var gLogger, _ = New("debug", "", "", log.LstdFlags|log.Lshortfile,1)
+var gLogger, _ = New("debug", "", "", log.LstdFlags|log.Lshortfile, 1)
 
 // It's dangerous to call the method on logging
 func Export(logger *Logger) {
@@ -611,7 +636,7 @@ func SRelease(a ...interface{}) {
 }
 
 func SWarning(a ...interface{}) {
-	gLogger.doSPrintf(warningLevel, printWarningLevel,  a)
+	gLogger.doSPrintf(warningLevel, printWarningLevel, a)
 }
 
 func SError(a ...interface{}) {
@@ -624,11 +649,12 @@ func SStack(a ...interface{}) {
 }
 
 func SFatal(a ...interface{}) {
-	gLogger.doSPrintf(fatalLevel, printFatalLevel,  a)
+	gLogger.doSPrintf(fatalLevel, printFatalLevel, a)
 }
 
-const timeFlag = syslog.Ldate|syslog.Ltime|syslog.Lmicroseconds
-func (logger *Logger) formatHeader(buf *Buffer,calldepth int,t *time.Time) {
+const timeFlag = syslog.Ldate | syslog.Ltime | syslog.Lmicroseconds
+
+func (logger *Logger) formatHeader(buf *Buffer, calldepth int, t *time.Time) {
 	var file string
 	var line int
 	if logger.flag&(syslog.Lshortfile|syslog.Llongfile) != 0 {
@@ -666,7 +692,7 @@ func (logger *Logger) formatHeader(buf *Buffer,calldepth int,t *time.Time) {
 
 			if logger.flag&syslog.Lmicroseconds != 0 {
 				buf.AppendByte('.')
-				buf.AppendInt(int64(t.Nanosecond()/1e3))
+				buf.AppendInt(int64(t.Nanosecond() / 1e3))
 			}
 			buf.AppendByte(' ')
 		}
